29fe81182e726660385a0fe35772f20d7d2e0d20
Alexander Wong <awwong1@ualberta.ca>
2019-01-29 09:50:02 -0700

    Added generic views

--- polls/views.py
+++ polls/views.py
@@ -1,20 +1,38 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 from .models import Choice, Question
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {"latest_question_list": latest_question_list}
+#     return render(request, "polls/index.html", context)
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
+
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/detail.html", {"question": question})
+
+class DetailView(generic.DetailView):
+    template_name = "polls/detail.html"
+    model = Question
+
+
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/results.html", {"question": question})
+
+class ResultsView(generic.DetailView):
+    template_name = "polls/results.html"
+    model = Question
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -30,4 +48,4 @@ def vote(request, question_id):
         selected_choice.save()
         # after POST data, this will prevent a user from posting
         # twice when they hit the back button
-        return HttpResponseRedirect(reverse("results", args=(question.id,)))
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
